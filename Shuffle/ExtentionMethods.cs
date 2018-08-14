a7298d7e9a2522249f91685caf66c38a7604851b
Tim Hall <Timmy@Tims-MacBook-Pro.local>
2018-08-14 21:46:38 +0100

    First Commit

---
+++ Shuffle/ExtentionMethods.cs
@@ -0,0 +1,34 @@
+﻿namespace Shuffle
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System;
+
+    public static class ExtensionMenthods
+    {
+        public static IEnumerable<TSource> Shuffle<TSource>(this IEnumerable<TSource> input)
+        {
+            Random rnd = new Random();
+
+            int uBound = input.Count() - 1;
+            int maxVal = uBound;
+            int ind;
+
+            List<TSource> inputList = input.ToList();
+            List<int> indexList = new List<int>(uBound + 1);
+            List<TSource> returnList = new List<TSource>(uBound + 1);
+
+            for (int i = 0; i <= uBound; i++) indexList.Add(i);
+            for (int i = 0; i <= uBound; i++)
+            {
+                ind = rnd.Next(maxVal);
+                returnList.Add(inputList[indexList[ind]]);
+                indexList.RemoveAt(ind);
+                maxVal--;
+            }
+
+            return returnList;
+
+        }
+    }
+}
